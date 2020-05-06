78af815488065849f73a19d9b30d8319334d026a
Jacob Knuth <JacobKnuth01@gmail.com>
2020-05-06 12:23:16 -0500

    Initial project setup

---
+++ Podfile
@@ -0,0 +1,10 @@
+
+platform :ios, '9.0'
+
+target 'maze' do
+ 
+  use_frameworks!
+
+  pod 'SwiftSocket'
+
+end
---
+++ Pods/Target Support Files/Pods-maze/Pods-maze-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_maze : NSObject
+@end
+@implementation PodsDummy_Pods_maze
+@end
